a352396ac8aac52dd8fc17e22e6f8f76ce22762e
ayushi jain08 <ayushijain0807@gmail.com>
2024-01-16 22:49:34 +0530

    add

--- src/Components/AddTask/AddTask.js
+++ src/Components/AddTask/AddTask.js
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
 const AddTask = () => {
-  const { addTask, task } = useTaskContext();
+  const { addTask } = useTaskContext();
   const [taskName, setTaskName] = useState("");
   const navigate = useNavigate();
 
